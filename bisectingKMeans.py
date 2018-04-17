3c49896091babb727873b9a880d0adea6f3637ac
bpowe1991 <36084040+bpowe1991@users.noreply.github.com>
2018-04-17 16:44:58 -0500

    added a plot function

--- bisectingKMeans.py
+++ bisectingKMeans.py
@@ -1,5 +1,6 @@
 import random
 import math
+import matplotlib.pyplot as pl
 
 def generate_points(seed):
     points = []
@@ -37,9 +38,45 @@ def calculate_distances(centroids, points):
     for each in manhattan_distances:
         print(each)
 
+def calculate_centroid(points):
+    sum_of_x = 0.0
+    sum_of_y = 0.0
+    x_coor = 0.0
+    y_coor = 0.0
+
+
+    for point in points:
+        sum_of_x += point[0]
+        sum_of_y += point[1]
+
+    x_coor = round((1/len(points))*sum_of_x, 2)
+    y_coor = round((1/len(points))*sum_of_y, 2)
+    centroid = (x_coor, y_coor)
+
+    return centroid
+
+def print_points(points):
+    x_values = []
+    y_values = []
+
+    for point in points:
+        x_values.append(point[0])
+        y_values.append(point[1])
+    
+    pl.plot(x_values, y_values, 'bo')
+    pl.axis([0.0, 100.0, 0.0, 100.0])
+    pl.show()
+    
 
 seed = 50
 point_list = generate_points(seed)
-print(point_list)
-centroids = [(50.0, 50.0)]
+print("Points:")
+for each in point_list:
+    print(each)
+print("Centroids:")
+centroids = [calculate_centroid(point_list)]
+for each in centroids:
+    print(each)
 calculate_distances(centroids, point_list)
+
+print_points(point_list)
